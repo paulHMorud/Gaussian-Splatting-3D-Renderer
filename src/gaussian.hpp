37eceadf432a35345da5333a0823a4beb5da0cd3
Paul Morud <paulmorud@gmail.com>
2026-04-07 13:10:03 +0200

    Commented out normals from parser, is not always in the ply files and are not needed for rendering

--- src/gaussian.hpp
+++ src/gaussian.hpp
@@ -10,9 +10,11 @@
 #include <pcl/point_types.h>
 #include <pcl/PCLPointCloud2.h>
 
+// Struct for holding the data of a single splat
+// Should be sent straight to the GPU when I implement spherical harmonics. 
 struct GaussianData {
     glm::vec3 position;     // x, y, z
-    glm::vec3 normal;       // normal_x, normal_y, normal_z
+    // glm::vec3 normal;       // normal_x, normal_y, normal_z
 
     glm::vec3 f_dc;         // f_dc_0, f_dc_1, f_dc_2
     std::array<float, 45> f_rest; // f_rest_0 ... f_rest_44
@@ -23,6 +25,9 @@ struct GaussianData {
     glm::vec4 rotation;     // rot_0, rot_1, rot_2, rot_3
 };
 
+
+// To be honest this should in no way be its own class.
+// Just wanted to see how to create classes in C++, should obviously just be a single function. 
 class GaussianLoader {
 private:
     pcl::PCLPointCloud2 point_cloud;
@@ -62,9 +67,9 @@ public:
         int y_i = findField("y");
         int z_i = findField("z");
 
-        int nx_i = findField("normal_x");
-        int ny_i = findField("normal_y");
-        int nz_i = findField("normal_z");
+        // int nx_i = findField("normal_x");
+        // int ny_i = findField("normal_y");
+        // int nz_i = findField("normal_z");
 
         int fdc0_i = findField("f_dc_0");
         int fdc1_i = findField("f_dc_1");
@@ -86,9 +91,9 @@ public:
         size_t y_offset = fields[y_i].offset;
         size_t z_offset = fields[z_i].offset;
 
-        size_t nx_offset = fields[nx_i].offset;
-        size_t ny_offset = fields[ny_i].offset;
-        size_t nz_offset = fields[nz_i].offset;
+        // size_t nx_offset = fields[nx_i].offset;
+        // size_t ny_offset = fields[ny_i].offset;
+        // size_t nz_offset = fields[nz_i].offset;
 
         size_t fdc0_offset = fields[fdc0_i].offset;
         size_t fdc1_offset = fields[fdc1_i].offset;
@@ -130,11 +135,11 @@ public:
                 readFloat(point, z_offset)
             );
 
-            g.normal = glm::vec3(
-                -readFloat(point, nx_offset),
-                -readFloat(point, ny_offset),
-                readFloat(point, nz_offset)
-            );
+            // g.normal = glm::vec3(
+            //     -readFloat(point, nx_offset),
+            //     -readFloat(point, ny_offset),
+            //     readFloat(point, nz_offset)
+            // );
 
             g.f_dc = glm::vec3(
                 readFloat(point, fdc0_offset),
